abd06f1d56c762dfeb1c77ffefb05d5a56831152
Mukhammad Nuril Haidar <nurilhaidar15@gmail.com>
2023-11-01 15:53:50 +0700

    Progress

--- uts/lib/main.dart
+++ uts/lib/main.dart
@@ -8,7 +8,8 @@ class myApp extends StatelessWidget {
   const myApp({super.key});
 
   @override
-  Widget build(BuildContext) {
+  // _myAppState createState() => _myAppState();
+  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Link Aja',
@@ -20,17 +21,23 @@ class myApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+/*class _myAppState extends State<myApp> {
+  int index = 0;
+
+  final List<Widget> _tabs = [
+    TabScreen(title: 'Tab 01', color: Colors.red),
+    TabScreen(title: 'Tab 02', color: Colors.green),
+    TabScreen(title: 'Tab 03', color: Colors.blue)
+  ];
 
   @override
-  Widget build(BuildContext) {
+  Widget build(BuildContext context) {
     return Scaffold(
       // backgroundColor: Colors.blue,
       body: Center(
         child: Text('Hello World'),
       ),
-      bottomNavigationBar: BottomAppBar(
+      /*bottomNavigationBar: BottomAppBar(
         child: Container(
           height: 60,
           // color: Colors.red,
@@ -204,17 +211,293 @@ class HomePage extends StatelessWidget {
           ),
         ),
       ),
+      */
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: index,
+        onTap: (index) {
+          setState(() {
+            index = index;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined), label: 'Home'),
+        ],
+      ),
 
-      /*bottomNavigationBar: BottomNavigationBar(
+      floatingActionButton: InkWell(
+        child: Container(
+          width: 50,
+          height: 50,
+          child: Icon(
+            Icons.qr_code_scanner,
+            color: Colors.white,
+          ),
+          decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black,
+                  blurRadius: 10,
+                  spreadRadius: 0,
+                  offset: Offset(0, 0),
+                )
+              ],
+              color: Colors.red,
+              borderRadius: BorderRadiusDirectional.circular(10),
+              border: Border.all(color: Colors.white, width: 3)),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
+*/
+/*class TabScreen extends StatelessWidget {
+  final String title;
+  final Color color;
+
+  TabScreen({required this.title, required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          color: color,
+          child: Center(
+            child: Text(
+              title,
+              style: TextStyle(fontSize: 24, color: Colors.white),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+*/
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // backgroundColor: Colors.blue,
+      body: Center(
+        child: Text('Hello World'),
+      ),
+      /*bottomNavigationBar: BottomAppBar(
+        child: Container(
+          height: 60,
+          // color: Colors.red,
+          child: Row(
+            children: [
+              Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.only(left: 20, right: 20),
+                width: 50,
+                height: 40,
+                // color: Colors.blue,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      alignment: Alignment.center,
+                      width: 30,
+                      height: 25,
+                      child: Icon(
+                        Icons.home_outlined,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Container(
+                      alignment: Alignment.center,
+                      width: 50,
+                      height: 15,
+                      child: Text(
+                        'Home',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 13,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.only(right: 15),
+                width: 50,
+                height: 40,
+                // color: Colors.blue,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      alignment: Alignment.center,
+                      width: 30,
+                      height: 25,
+                      child: Icon(
+                        Icons.description_outlined,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Container(
+                      alignment: Alignment.center,
+                      width: 50,
+                      height: 15,
+                      child: Text(
+                        'History',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 13,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                width: 50,
+                height: 40,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      alignment: Alignment.center,
+                      width: 30,
+                      height: 25,
+                    ),
+                    Container(
+                      alignment: Alignment.center,
+                      width: 50,
+                      height: 15,
+                      child: Text(
+                        'Pay',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 13,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 15),
+                alignment: Alignment.center,
+                width: 50,
+                height: 40,
+                // color: Colors.blue,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      alignment: Alignment.center,
+                      width: 30,
+                      height: 25,
+                      child: Icon(
+                        Icons.email_outlined,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Container(
+                      alignment: Alignment.center,
+                      width: 50,
+                      height: 15,
+                      child: Text(
+                        'Inbox',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 13,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.only(left: 20),
+                width: 50,
+                height: 40,
+                // color: Colors.blue,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      alignment: Alignment.center,
+                      width: 30,
+                      height: 25,
+                      child: Icon(
+                        Icons.person_outline_rounded,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Container(
+                      alignment: Alignment.center,
+                      width: 50,
+                      height: 15,
+                      child: Text(
+                        'Account',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 13,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      */
+      bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         items: [
           BottomNavigationBarItem(
             icon: Icon(Icons.home_outlined),
-            label: 
-          )
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.description_outlined),
+            label: 'History',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(null),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.email_outlined),
+            label: 'Inbox',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline_rounded),
+            label: 'Account',
+          ),
         ],
+        selectedLabelStyle: TextStyle(
+          fontSize: 13,
+          fontWeight: FontWeight.bold,
+        ),
+        unselectedLabelStyle: TextStyle(
+          fontSize: 13,
+          fontWeight: FontWeight.bold,
+        ),
       ),
-      */
+
       floatingActionButton: InkWell(
         child: Container(
           width: 50,
